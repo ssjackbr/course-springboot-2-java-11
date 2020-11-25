4058723fb478ffaa2125d096f30972c0ab2ae5c9
Jackson Rodrigues <jjr0910@gmail.com>
2020-11-25 19:13:14 -0300

    User entity and resource

---
+++ src/main/java/br/com/igni/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package br.com.igni.course.resources;
+
+import br.com.igni.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User (1L,"Maria","maria@gmail.com","31999999999","12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
