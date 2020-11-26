a640cd84dc3e6793f2f51585e204a645ceab45d5
Jackson Rodrigues <jjr0910@gmail.com>
2020-11-25 22:59:18 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/igni/course/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.igni.course.services;
+
+import br.com.igni.course.entities.User;
+import br.com.igni.course.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById (Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
